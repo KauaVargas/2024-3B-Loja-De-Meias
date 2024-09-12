0b21a398871e25afa9dbaa52d5c218034faf08d5
Kauã Vinícius <106679339+KauaVargas@users.noreply.github.com>
2024-09-12 10:33:08 -0300

    Aula 12/09

--- app.py
+++ app.py
@@ -1,18 +1,34 @@
-print('Meias Hoahi\n')
+import os
 
-print('1. Cadastro de meias')
-print('2. Listar meias')
-print('3. Ativar meias')
-print ('4. Sair da aplicação')
+def mostra_titulo():
 
-opcao_escolhida = int(input('Escolha uma opção:'))
-print('Você escolheu a opção: ', opcao_escolhida)
+    print('𝓜𝓮𝓲𝓪𝓼 𝓗𝓸𝓪𝓱𝓲\n')
 
-if opcao_escolhida == 1:
-    print('Cadastrar meias')
-elif opcao_escolhida == 2:
-    print('Listar meias')
-elif opcao_escolhida == 3:
-    print('Ativar/Desativar meias')
-else:
-    print('Saindo da aplicação')
+def mostra_escolhas():
+    print('1. Cadastro de meias')
+    print('2. Listar meias')
+    print('3. Ativar meias')
+    print ('4. Sair da aplicação')
+
+def escolhe_opcao():
+    opcao_escolhida = int(input('Escolha uma opção:'))
+    print('Você escolheu a opção: ', opcao_escolhida)
+
+    def finalizar_programa():
+        os.system('cls')
+        print('Finalizando programa')
+
+    if opcao_escolhida == 1:
+        print('Cadastrar meias')
+    elif opcao_escolhida == 2:
+        print('Listar meias')
+    elif opcao_escolhida == 3:
+        print('Ativar/Desativar meias')
+    else:
+        finalizar_programa()
+
+def main():
+    mostra_titulo()
+    mostra_escolhas()
+    escolhe_opcao()
+    
